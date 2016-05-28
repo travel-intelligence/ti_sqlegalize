4f7452feea59c6811f02bfb75cbc6c295889db8e
Emmanuel Bastien <os@ebastien.name>
2016-05-28 17:31:53 +0200

    Spec for domain model.

--- spec/models/ti_sqlegalize/domain_spec.rb
+++ spec/models/ti_sqlegalize/domain_spec.rb
@@ -1,1 +1,18 @@
 # encoding: utf-8
+require 'rails_helper'
+
+describe TiSqlegalize::Domain do
+
+  before(:each) { mock_domains }
+
+  it "complains on unknown domain" do
+    expect do
+      TiSqlegalize::Domain.find 'not_a_domain'
+    end.to raise_error(TiSqlegalize::Domain::UnknownDomain)
+  end
+
+  it "fetches an existing domain" do
+    domain = Fabricate(:domain)
+    expect(TiSqlegalize::Domain.find domain.id).to eq(domain)
+  end
+end
